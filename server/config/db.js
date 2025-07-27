043933fa5b52008fe071050da98cd05377cf27e6
Willoandru <wilsonandrescriollo@gmail.com>
2025-07-27 13:02:57 -0500

    Adaptacoin Railway

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,15 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port: process.env.DB_PORT || 3306,
-});
+const db = new Sequelize(
+  process.env.MYSQLDATABASE || process.env.DB_NAME,
+  process.env.MYSQLUSER || process.env.DB_USER,
+  process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  {
+    host: process.env.MYSQLHOST || process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  }
+);
 
 export default db
