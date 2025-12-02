ae1fe7f21bc64f67b70d49be1619301df03f972e
Willoandru <wilsonandrescriollo@gmail.com>
2025-12-02 08:46:14 -0500

    Crud completo con la implementacion de imagenes por url

--- client/src/components/ColumProducts.jsx
+++ client/src/components/ColumProducts.jsx
@@ -1,43 +1,43 @@
-import { MdDeleteForever } from 'react-icons/md';
-import axios from 'axios';
-import Swal from 'sweetalert2';
+import { MdDeleteForever } from "react-icons/md";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const URI = import.meta.env.VITE_REACT_APP_API_URL;
 
 function ColumProducts({ product }) {
-  const imageUrl = product.image ? URL.createObjectURL(new Blob([new Uint8Array(product.image.data)], { type: 'image/jpeg' })) : '';
-
   const handleDeleteClick = async () => {
     const result = await Swal.fire({
       title: `Are you sure you want to delete the product ${product.name}?`,
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'Cancel'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "Cancel",
     });
 
     if (result.isConfirmed) {
       await axios.delete(`${URI}/deleteProduct/${product.id}`);
       window.location.reload();
     }
-  }
+  };
 
   return (
     <tr>
-      <td className='center'>{product.id}</td>
+      <td className="center">{product.id}</td>
       <td>{product.name}</td>
-      <td><img src={imageUrl} alt={product.name} /></td>
+      <td>
+        <img src={product.image} alt={product.name} />
+      </td>
       <td>{product.description}</td>
       <td>{product.price}</td>
       <td>{product.quantity}</td>
       <td>{product.categoryId}</td>
       <td>{product.createdAt}</td>
       <td>{product.updatedAt}</td>
-      <td className='center'>
+      <td className="center">
         <button onClick={handleDeleteClick}>
-          <MdDeleteForever className='icon' />
+          <MdDeleteForever className="icon" />
         </button>
       </td>
     </tr>
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,145 +1,169 @@
 import ProductModel from "../models/productModel.js";
-import sharp from 'sharp'
+import axios from "axios";
+import FormData from "form-data";
 
 export const getAllProducts = async (req, res) => {
-    try {
-        const products = await ProductModel.findAll();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const products = await ProductModel.findAll();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const getProduct = async (req, res) => {
-    try {
-        const products = await ProductModel.findByPk(req.params.id);
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const products = await ProductModel.findByPk(req.params.id);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const getProductsIdNames = async (req, res) => {
-    try {
-        const products = await ProductModel.findAll();
-        const productsIdNames = products.map(product => ({ id: product.id, name: product.name }));
+  try {
+    const products = await ProductModel.findAll();
+    const productsIdNames = products.map((product) => ({
+      id: product.id,
+      name: product.name,
+    }));
 
-        res.status(200).json(productsIdNames);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    res.status(200).json(productsIdNames);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const getProductsById = async (req, res) => {
-    try {
-        const { ids } = req.body;
-        const products = await ProductModel.findAll({ where: { id: ids } })
-        return res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}
+  try {
+    const { ids } = req.body;
+    const products = await ProductModel.findAll({ where: { id: ids } });
+    return res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export const publishProduct = async (req, res) => {
-    const { name, description, price, quantity, category } = req.body;
-    const image = req.file;
+  const { name, description, price, quantity, category } = req.body;
+  const image = req.file;
 
-    try {
-        const optimizedImageBuffer = await sharp(image.buffer)
-            .resize({ width: 800 })
-            .jpeg({ quality: 80 })
-            .toBuffer();
+  if (!image) {
+    return res.status(400).json({ error: "Image required" });
+  }
 
-        await ProductModel.create({
-            name: name,
-            image: optimizedImageBuffer,
-            description: description,
-            price: price,
-            quantity: quantity,
-            categoryId: category,
-        })
+  try {
+    const formData = new FormData();
+    formData.append("image", image.buffer.toString("base64"));
 
-        return res.status(200).json({ message: "Published product" });
-    } catch (error) {
-        console.error(error.message);
-        return res.status(500).json({ error: error.message })
-    }
-}
+    const imgbbResponse = await axios.post(
+      `https://api.imgbb.com/1/upload?key=${process.env.IMGBB_API_KEY}`,
+      formData
+    );
+
+    const imageUrl = imgbbResponse.data.data.url;
+
+    await ProductModel.create({
+      name,
+      image: imageUrl,
+      description,
+      price,
+      quantity,
+      categoryId: category,
+    });
+
+    return res.status(200).json({ message: "Published product", imageUrl });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 export const updateProduct = async (req, res) => {
-    const { id } = req.params;
-    const { name, description, price, quantity, category } = req.body;
-    const image = req.file;
+  const { id } = req.params;
+  const { name, description, price, quantity, category } = req.body;
+  const image = req.file;
 
-    try {
-        const updatedFields = {
-            name: name,
-            description: description,
-            price: price,
-            quantity: quantity,
-            categoryId: category,
-        };
+  try {
+    const updatedFields = {
+      name,
+      description,
+      price,
+      quantity,
+      categoryId: category,
+    };
 
-        if (image) {
-            const optimizedImageBuffer = await sharp(image.buffer)
-                .resize({ width: 800 })
-                .jpeg({ quality: 80 })
-                .toBuffer();
-            updatedFields.image = optimizedImageBuffer;
-        }
+    // Si llegó una imagen nueva → subirla a imgbb
+    if (image) {
+      const formData = new FormData();
+      formData.append("image", image.buffer.toString("base64"));
 
-        await ProductModel.update(updatedFields, {
-            where: { id: id }
-        });
+      const imgbbResponse = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${process.env.IMGBB_API_KEY}`,
+        formData
+      );
 
-        return res.status(200).json({ message: "Updated product" });
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
+      const imageUrl = imgbbResponse.data.data.url;
+      updatedFields.image = imageUrl; // ahora guardamos URL
     }
-}
+
+    await ProductModel.update(updatedFields, {
+      where: { id },
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Updated product", imageUpdated: !!image });
+  } catch (error) {
+    console.error("IMGBB ERROR:", error.response?.data || error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 export const deleteProduct = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const product = await ProductModel.findOne({ where: { id } })
+  try {
+    const product = await ProductModel.findOne({ where: { id } });
 
-        await product.destroy();
+    await product.destroy();
 
-        return res.status(200).json({ message: "Product removed" });
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-}
+    return res.status(200).json({ message: "Product removed" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 export const getProductsCount = async (req, res) => {
-    try {
-        const productsCount = await ProductModel.count();
-        return res.status(200).json(productsCount)
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-}
+  try {
+    const productsCount = await ProductModel.count();
+    return res.status(200).json(productsCount);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 export const getLatest = async (req, res) => {
-    try {
-        const latest = await ProductModel.findAll({
-            order: [['id', 'DESC']],
-            limit: 15
-        });
-        return res.status(200).json(latest)
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-}
+  try {
+    const latest = await ProductModel.findAll({
+      order: [["id", "DESC"]],
+      limit: 15,
+    });
+    return res.status(200).json(latest);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 export const getForDay = async (req, res) => {
-    const { categoryId } = req.body;
+  const { categoryId } = req.body;
 
-    try {
-        const forDay = await ProductModel.findAll({ where: { categoryId: categoryId } });
-        return res.status(200).json(forDay)
-    } catch (error) {
-        return res.status(500).json({ error: error.message })
-    }
-}
+  try {
+    const forDay = await ProductModel.findAll({
+      where: { categoryId: categoryId },
+    });
+    return res.status(200).json(forDay);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
