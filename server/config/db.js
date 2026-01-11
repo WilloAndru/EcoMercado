f4c39de5c00b29c3c1a56e978f3576ffac44a0e0
Willoandru <wilsonandrescriollo@gmail.com>
2026-01-11 09:57:47 -0500

    Prueba con NODE_ENV, para cambiar entre produccion y desarrollo

--- server/config/db.js
+++ server/config/db.js
@@ -1,27 +1,25 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
 
-const isProduction =
-  process.env.DATABASE_URL && !process.env.DATABASE_URL.includes("localhost");
-
-const db = new Sequelize(process.env.DATABASE_URL || process.env.PGDATABASE, {
-  dialect: "postgres",
-  protocol: "postgres",
-  host: process.env.PGHOST,
-  port: Number(process.env.PGPORT) || 5432,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  logging: false,
-  dialectOptions: isProduction
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      }
-    : {},
-});
+const db = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT) || 5432,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+  }
+);
 
 export default db;
